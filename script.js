b60718c3de371e7cdd17c3128ff31d6f3898ac3d
Angela8306 <108442881+Angela8306@users.noreply.github.com>
2024-03-14 23:37:55 -0500

    failed attempt at lines

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-const lines = [];
-const angles = [0, 30, 45, 60, 90, 120, 135, 150, 180, 210, 225, 240, 270, 300, 315, 330, 360];
-
-function make_lines(line_list, angle_list) {
-    let original_line = document.getElementById('original-line');
-    for (const angle of angle_list) {
-        var new_line = $.extend(true, {}, original_line);
-        new_line.setAttribute("transform", "rotate(${angle})");
-        line_list.push(new_line);
+function make_lines() {
+    let angle_list = [0, 30, 45, 60, 90, 120, 135, 150, 180, 210, 225, 240, 270, 300, 315, 330, 360];
+    let original = document.getElementById('original-line');
+    let line_container = document.getElementById('line-container')
+    for (var angle of angle_list) {
+        let new_line = $.extend(true, {}, original).setAttribute("transform", `rotate(${angle})`)
+        document.line_container.appendChild(new_line)
     }
 }
-
-// function draw_lines_to_doc(line_list) {
-//     for (const line of line_list) {
-
-//     }
-// }
-
-make_lines(lines, angles);
-console.log("hello world")
