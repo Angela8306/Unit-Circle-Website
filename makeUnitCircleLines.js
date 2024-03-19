c9dc09db73775543b5f0285372db8eb159b4f278
Angela8306 <angelaml123123@gmail.com>
2024-03-19 14:52:43 -0500

    got unit circle lines drawn correctly

---
+++ makeUnitCircleLines.js
@@ -0,0 +1,19 @@
+const originalLine = document.getElementById("line-0");
+
+const angles = [30, 45, 60, 90, 120, 135, 150, 180, 210, 225, 240, 270, 300, 315, 330];
+
+angles.forEach(makeLines) 
+
+function makeLines(angle) {
+    const newLine = originalLine.cloneNode(true);
+
+    newLine.setAttribute("id", "line-" + angle)
+
+    newLine.setAttribute("transform", "rotate(" + -angle + " 250 250)");
+
+    newLine.setAttribute("style", "stroke: rgb(79, 111, 82); stroke-width: 1.5;");
+
+    // Append the cloned line to the SVG container
+    document.getElementById("line-container").appendChild(newLine);
+    
+};
--- script.js
+++
@@ -1,9 +0,0 @@
-function make_lines() {
-    let angle_list = [0, 30, 45, 60, 90, 120, 135, 150, 180, 210, 225, 240, 270, 300, 315, 330, 360];
-    let original = document.getElementById('original-line');
-    let line_container = document.getElementById('line-container')
-    for (var angle of angle_list) {
-        let new_line = $.extend(true, {}, original).setAttribute("transform", `rotate(${angle})`)
-        document.line_container.appendChild(new_line)
-    }
-}
